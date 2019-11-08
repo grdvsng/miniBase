5e4f488560f7228b27a73c62235217ea6e89b53c
grdvsng <grdvsng@gmail.com>
2019-11-08 18:25:57 +0300

    update\

--- install.sh
+++ install.sh
@@ -1,42 +1,80 @@
 #!/bin/bash
 
-
 redisUrl="http://download.redis.io/releases/redis-5.0.5.tar.gz";
+logUrl="./logs/install.log"
+basicFiles=("./temp" "./redis-5.0.5" "./venv")
+basicUrl=$PWD
 
-if [ -f "$PWD/temp" ]; then
-    rm -R "$PWD/temp";
-fi
+logger()
+{
+    msg="
+'user': '$USER'
+'Time': '`date -u`'
+'Event': '$1'
+------------------------------------
+"
 
-if [ -f "$PWD/redis-5.0.5" ]; then
-    rm -R "$PWD/redis-5.0.5";
-fi
+    clear
 
-timestamp=(`date`)
-echo "$timestamp\nDownload and unpack...">> ./logs/install.log
-sleep 5;
+    printf "$msg"
+    printf "$msg" >> $logUrl
 
-mkdir "$PWD/temp";
+    sleep 5
+}
+
+
+#Remove old instalation
+for item in ${basicFiles[*]}; do
+    if [ -d $item ]; then
+        rm -R $item
+        logger "Remove: $item"
+    fi
+done
+
+
+#Virtual env install
+logger "Install python3-pip"
+sudo apt-get install python3-pip
+
+logger "Install virtualenv"
+sudo pip3 install virtualenv
+
+logger "Create virtualenv"
+virtualenv venv
+source ./venv/bin/activate
+
+logger "Install plugins"
+pip3 install redis || pip install redis
+pip3 install flask || pip install flask
+
+
+#Redis install
+logger "Download redis"
+mkdir "./temp";
 wget $redisUrl -O "$PWD/temp/redis.gz";
+
+logger "Extract redis"
 tar --extract -f "$PWD/temp/redis.gz" -C "$PWD";
+
+logger "Remove temp files for redis"
 rm -R "$PWD/temp";
-clear;
 
-
-echo "$timestampStart install redis...">>./logs/install.log
-sleep 5;
-
-cd "redis-5.0.5";
-make;
+logger "Install redis"
+cd "redis-5.0.5"
+make
 sudo cp "src/redis-server" /usr/local/bin/;
 sudo cp "src/redis-cli" /usr/local/bin/;
-clear;
+cd $basicUrl
+
+logger "Base preinstalation"
+gnome-terminal -x sh -c ". ./start_redis.sh; bash"
+logger "Base configurate"
+python3 ./install_db_presetting.py
 
 
-echo "$timestamp\nStartPresseting redis...">>../logs/install.log
-sleep 5;
+#Continue
+logger "Continue"
+. ./stop_redis.sh
+cd $basicUrl
+deactivate
 
-cd ..
-gnome-terminal -- "start_redis.sh" && python3 "install_db_presetting.py"
-
-echo "$timestamp\nContinue...">>./logs/install.log
-sleep 5;
