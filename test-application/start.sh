0005550a904c3ac7e0e9d72b3c369ca445445637
grdvsng <grdvsng@gmail.com>
2019-11-15 23:26:41 +0300

    update

--- test-application/start.sh
+++ test-application/start.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
+
+#Start server
 source $PWD/venv/bin/activate
 gnome-terminal -x sh -c "redis-server"
 python3 -b app.py
+
+
+#Stop server
 deactivate
-redis-cli shutdown
+sudo redis-cli shutdown
+
+
+#check than redis died
+redisPids=$(ps aux | grep -i "redis" | awk '{print $11"__"$2}')
+for item in ${redisPids[*]}; do
+    name=$(awk -F__ '{print $1}' <<<  $item)
+    pid=$(awk -F__ '{print $2}' <<<  $item)
+
+    if [[ $name != *"grep"* ]] && [[ $pid != "" ]]; then
+        echo "kill $name $pid"
+        sudo kill -9 $pid
+    fi
+done
