e7db525cdde8374690d00ec238ea480a0fbb0f85
grdvsng <grdvsng@gmail.com>
2019-11-15 21:57:23 +0300

    update

--- test-application/app.py
+++ test-application/app.py
@@ -5,5 +5,5 @@ logCfgPath    = "lib/log_config.json" # not require
 app           = MiniBase(serverCfgPAth, logCfgPath)
 
 if __name__ == '__main__':
-    FLASK_APP=app
+    FLASK_APP = app
     app.start()
