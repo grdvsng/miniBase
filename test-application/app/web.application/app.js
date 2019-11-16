1f6d715d714daf896e947ef14af4f6effc609f84
grdvsng <grdvsng@gmail.com>
2019-11-16 19:48:20 +0300

    update

--- test-application/app/web.application/app.js
+++ test-application/app/web.application/app.js
@@ -1,9 +1,21 @@
-var app = 
+/**
+ * Your web Application configuration
+ * @namespace
+ * @const
+ * @property {string} [name=MiniBase]- Name of application (will push in page title)
+ * @property {string} [ico=null] - path of suit favicon
+ * @property {Array.<String>} [elements=null] - name of elements than will use for application, if null will connecting all of them
+ * @property {Array.<String>} pages - name of your created page in ./web.application/pages
+ * @property {Array.<String>} [my_styles=null] - name of your created style in ./web.application/styles
+ * @property {String} [index=null] - view will open with index page
+ * @property {String} [style="default"] - application main css style.
+ */
+var app =
 {
    "name": "MiniBase",
    "ico": "media/favicon.ico",
    "elements": null, // all, if not write, ["Element1", ...]
-   
+
    // only in ./pages
    "pages":
    [
@@ -11,9 +23,9 @@ var app =
        "search_user_page",
        "push_user_page"
    ],
-   
+
    // if user should past style in "./styles" , in Array push only file name
-   "my_styles": 
+   "my_styles":
    [
        "main"
    ],
